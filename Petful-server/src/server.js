2024e839e27afbb6e9b4843512edd3d7ea19b62f
Daniel <herrnichols@gmail.com>
2019-11-15 10:25:05 -0500

    initial deply

--- Petful-server/src/server.js
+++ Petful-server/src/server.js
@@ -1,11 +1,39 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { node_env } = require('./config');
+const { CLIENT_ORIGIN } = require('../config');
+
+module.exports = {
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  PORT: process.env.port || 8080,
+};
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN,
+  })
+);
+
+const cat = {
+  imageURL:
+    'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+  imageDescription:
+    'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street',
+};
+
+app.get('/api/cat', (req, res) => {
+  res.json(cat);
+});
 
 // Catch-all 404
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -13,14 +41,14 @@ app.use(function (req, res, next) {
 
 // Catch-all Error handler
 // Add NODE_ENV check to prevent stacktrace leak
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.json({
     message: err.message,
-    error: app.get('env') === 'development' ? err : {}
+    error: app.get('env') === 'development' ? err : {},
   });
 });
 
-app.listen(8080,()=>{
+app.listen(8080, () => {
   console.log('Serving on 8080');
 });
